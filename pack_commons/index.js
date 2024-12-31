8dfa6df2fb163285de6e3f58d4dc8cf0c1789e78
Xmas <github@xmas.social>
2024-12-31 18:30:23 +0530

    FIX - Make copy recursive

--- pack_commons/index.js
+++ pack_commons/index.js
@@ -5,15 +5,13 @@ const config = fs.existsSync(`data/pack_commons/commons.json`) ? JSON.parse(fs.r
 
 glob("data/pack_commons/**/!(commons.json)", null, function (err, files) {
     for (path of files) {
-        const name = path.split("/").pop()
-        fs.cpSync(path, `BP/${name}`, {recursive: true})
-        fs.cpSync(path, `RP/${name}`, {recursive: true})
+        fs.cpSync(path, `BP/${path}`, {recursive: true})
+        fs.cpSync(path, `RP/${path}`, {recursive: true})
     }
 })
 
 for (file of config.common_files) {
     const path = `../../${file}`
-    const name = path.split("/").pop()
-    fs.cpSync(path, `BP/${name}`, {recursive: true})
-    fs.cpSync(path, `RP/${name}`, {recursive: true})
+    fs.cpSync(path, `BP/${path}`, {recursive: true})
+    fs.cpSync(path, `RP/${path}`, {recursive: true})
 }
