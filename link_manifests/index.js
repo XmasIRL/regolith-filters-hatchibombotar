8b8881a75cec40d7647c6faddca95338ccea3818
Xmas <github@xmas.social>
2024-12-31 15:44:16 +0530

    Add try-catch on filter fail

--- link_manifests/index.js
+++ link_manifests/index.js
@@ -1,12 +1,16 @@
 const fs = require("fs")
 
-const bp = JSON.parse(fs.readFileSync("BP/manifest.json"))
-const rp = JSON.parse(fs.readFileSync("RP/manifest.json"))
+try {
 
-bp.dependencies = bp.dependencies?.filter( d => !d.uuid ) ?? []
-rp.dependencies = rp.dependencies?.filter( d => !d.uuid ) ?? []
-bp.dependencies.push( { "uuid": rp.header.uuid, "version": rp.header.version } )
-rp.dependencies.push( { "uuid": bp.header.uuid, "version": bp.header.version } )
+  const bp = JSON.parse(fs.readFileSync("BP/manifest.json"))
+  const rp = JSON.parse(fs.readFileSync("RP/manifest.json"))
 
-fs.writeFileSync("RP/manifest.json", JSON.stringify(rp, null, 2))
-fs.writeFileSync("BP/manifest.json", JSON.stringify(bp, null, 2))
+  bp.dependencies = bp.dependencies?.filter( d => !d.uuid ) ?? []
+  rp.dependencies = rp.dependencies?.filter( d => !d.uuid ) ?? []
+  bp.dependencies.push( { "uuid": rp.header.uuid, "version": rp.header.version } )
+  rp.dependencies.push( { "uuid": bp.header.uuid, "version": bp.header.version } )
+
+  fs.writeFileSync("RP/manifest.json", JSON.stringify(rp, null, 2))
+  fs.writeFileSync("BP/manifest.json", JSON.stringify(bp, null, 2))
+
+} catch(e) { console.error("Link Manifest Failed!" + e); }
